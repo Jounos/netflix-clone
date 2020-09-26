db6f6b66601ebb4b7672d75166672d8a1bb490be
Geovanny Neves Liberato <gnliberato@outlook.com>
2020-09-26 00:30:01 -0300

    Implementado loading da página

--- src/App.js
+++ src/App.js
@@ -6,17 +6,17 @@ import FeaturedMovie from './components/FeaturedMovie';
 import Header from './components/Header';
 
 export default () => {
-	
+
 	const [movieList, setMovieList] = useState([]);
 	const [featuredData, setFeaturedData] = useState(null);
 	const [blackHeader, setBlackHeader] = useState(true);
-	useEffect(()=> {
-		const loadAll = async () =>{
+	useEffect(() => {
+		const loadAll = async () => {
 			let list = await Tmdb.getHomeList();
 			setMovieList(list);
-		
-			let originals = list.filter(i=>i.slug === 'originals');
-			let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length -1));
+
+			let originals = list.filter(i => i.slug === 'originals');
+			let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length - 1));
 			let chosen = originals[0].items.results[randomChosen];
 
 			let chosenInfo = await Tmdb.getMovieInfo(chosen.id, 'tv');
@@ -43,21 +43,26 @@ export default () => {
 
 	return (
 		<div className="page">
-			<Header black={blackHeader}/>
+			<Header black={blackHeader} />
 			{featuredData &&
 				<FeaturedMovie item={featuredData} />
 			}
 			<section className="lists">
-				{movieList.map((item, key) =>(
-					<MovieRow key={key} title={item.title} items={item.items}/>
+				{movieList.map((item, key) => (
+					<MovieRow key={key} title={item.title} items={item.items} />
 				))}
 			</section>
 
 			<footer>
-				Feito com por Geovanny Neves Liberato<br/>
-				Direitos de imagem para netflix<br/>
+				Feito com por Geovanny Neves Liberato<br />
+				Direitos de imagem para netflix<br />
 				Dados pegos do site Themoviedb.org
 			</footer>
+			{movieList.length <= 0 &&
+				<div className="loading">
+					<img src="https://cdn.lowgif.com/small/0534e2a412eeb281-the-counterintuitive-tech-behind-netflix-s-worldwide.gif" alt="carregando" />
+				</div>
+			}
 		</div>
 	);
 }
